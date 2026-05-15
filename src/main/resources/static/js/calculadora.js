6f3470e65848452dd98260a16cd52484033e47d5
Joshua2025670 <jreyesr2101@alumno.ipn.mx>
2026-05-15 16:49:50 -0600

    Update calculadora.js

--- src/main/resources/static/js/calculadora.js
+++ src/main/resources/static/js/calculadora.js
@@ -1,34 +1,33 @@
-function suma(){
-    //document = pagina = dpcumentos cientificos en los 90's
-    let n1=document.getElementById("n1").value;
-    let n2=document.getElementById("n2").value;
-    let resultado= parseInt(n1)+parseInt(n2);
-    //Accedemos al parrafo que tiene un id de resultado y modificamos su contenido
-    document.getElementById("resultado").innerHTML="Resultado: "+resultado;
+function setDigito(digito){
+    let display = document.getElementById('display');
+    display.value=display.value + digito;
 }
-
-function resta(){
-    let n1=document.getElementById("n1").value;
-    let n2=document.getElementById("n2").value;
-    let resultado= n1-n2;
-    document.getElementById("resultado").innerHTML="Resultado: "+resultado;
-}
-
-function multiplicacion(){
-    let n1=document.getElementById("n1").value;
-    let n2=document.getElementById("n2").value;
-    let resultado= n1*n2;
-    document.getElementById("resultado").innerHTML="Resultado: "+resultado;
-}
-
-function div(){
-    let n1=document.getElementById("n1").value;
-    let n2=document.getElementById("n2").value;
-    if(n2 != 0) {
-        let resultado = n1/n2;
-        document.getElementById("resultado").innerHTML="Resultado: "+resultado;
+function setOperador(operador){
+    let display = document.getElementById('display');
+    let valorA = display.value;
+    let ultimoCar = valorA.slice(-1);
+    let operadores = ['+', '**', '*', '-', '/'];
+    if(operadores.includes(ultimoCar))
+    {
+        display.value = valorA.slice(0,-1) + operador;
     }
     else
-        document.getElementById("resultado").innerHTML="Imposible dividir entre 0";
-
+        display.value= valorA + operador;
+}
+function borrar(){
+    let display = document.getElementById('display');
+    display.value='';
+}
+function obtenerRes(){
+    //document = pagina = dpcumentos cientificos en los 90's
+    let display = document.getElementById('display');
+    display.value = eval(display.value);
+}
+function Raiz(){
+    let display = document.getElementById('display');
+    display.value = Math.sqrt(parseFloat(display.value));
+}
+function log(){
+    let display = document.getElementById('display');
+    display.value = Math.log10(parseFloat(display.value));
 }
